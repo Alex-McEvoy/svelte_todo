8983eaa83220183ec2a5759ed2634572961b77d3
Alex McEvoy <Alex.McEvoy@hearst.com>
2025-05-03 15:09:48 -0600

    feat: add loading indicator for create event with form progressive enhancement

--- src/routes/newevent/+page.svelte
+++ src/routes/newevent/+page.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 
+	let loading = $state(false);
 </script>
 
-<form method="POST">
-    <!-- form for creating new event -->
-    <label for="title">Title</label>
-    <input type="text" id="title" name="title" required>
-    <textarea id="description" name="description" rows="4" cols="50" placeholder="Description"></textarea>
-    <label for="date">Date</label>
-    <input type="datetime-local" id="date" name="date" required>
-    <button type="submit">Create Event</button>
+<form
+	method="POST"
+	use:enhance={() => {
+		loading = true;
+
+		return async ({ update }) => {
+			await update();
+
+			loading = false;
+		};
+	}}
+>
+	<!-- form for creating new event -->
+	<label for="title">Title</label>
+	<input type="text" id="title" name="title" required disabled={loading} />
+	<textarea
+		id="description"
+		name="description"
+		rows="4"
+		cols="50"
+		placeholder="Description"
+		disabled={loading}
+	></textarea>
+	<label for="date">Date</label>
+	<input type="datetime-local" id="date" name="date" required disabled={loading} />
+	<button type="submit">Create Event</button>
 </form>
+
+{#if loading}
+	<span>Loading.....</span>
+{/if}
